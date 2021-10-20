daf2ee2930078b7e455702b33ab14dfc8d59b7b4
Matheus Martins <matheusmartinsnascimento@gmail.com>
2021-10-20 12:45:44 -0300

    feito desafio do relogio

---
+++ sprint-4-front-end/Desafios/exemplo_relogio/relogio_app/src/App.js
@@ -0,0 +1,76 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function DataFormatada(props){
+  return <h2>Horário Atual: {props.date.toLocaleTimeString()}</h2>
+} 
+
+//componente de classe
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      date : new Date(),
+      pausado : false
+    };
+  };
+
+  thick(){
+    if (this.state.pausado === false) {
+      this.setState({
+        date : new Date()
+      })
+    }
+  }
+
+  //ciclo de vida de nascimento
+  componentDidMount(){
+    this.timerID = setInterval( () => {
+      this.thick()
+    }, 1000 )
+  };
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  };
+
+  pausar(){
+    this.setState({
+      pausado : true
+    })
+    console.log('Relógio ' + this.timerID + ' pausado');
+  }
+
+  retomar(){
+    this.setState({
+      pausado : false
+    })
+    console.log('Relógio retomado!')
+    console.log('Agora sou o relógio ' + this.timerID);
+  }
+
+  render(){
+    return(
+    <div>
+      <h1>Relógio</h1>
+      <DataFormatada date={this.state.date} />
+      <button className="Pausar" onClick={() => this.pausar()}>PAUSAR</button>
+      <button classname="Retomar" onClick={() => this.retomar()}>RETOMAR</button>
+    </div>
+    )
+  }
+}
+
+//componente funcional
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Clock />
+      </header>
+    </div> 
+  );
+}
+
+export default App;
